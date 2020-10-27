e9a97bc19d6a902329dd53e040da0981f13e7576
marcusvilete <marcusvilete@gmail.com>
2020-10-27 20:56:10 -0300

    Criando a cena base

---
+++ src/warrior-the-rise/Assets/Scripts/LevelController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    public Camera cam;
+    public Transform playableArea;
+    public Transform playerPrefab;
+
+    void Awake()
+    {
+        //Setup the scene
+        
+        //TODO: Not in play mode yet!
+
+        //Force portrait mode
+        ForcePortrait();
+
+        //1- Set camera to safe area
+        SetCamera();
+
+        //2- Scale playable area
+        ScalePlayableArea();
+
+        //
+        //3- Spawn Player
+        SpawnPlayer();
+        //
+
+        //
+        //4- Load Level data
+        LoadLevelData();
+
+        //TODO: Set to playmode!
+    }
+
+    private void ForcePortrait()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
+
+    private void SetCamera()
+    {
+        float width = (float)Screen.safeArea.width / Screen.width;
+        float height = (float)Screen.safeArea.height / Screen.height;
+        float x = (Screen.safeArea.x / Screen.width);
+        float y = (Screen.safeArea.y / Screen.height);
+        cam.rect = new Rect(x, y, width, height);
+    }
+
+    private void ScalePlayableArea()
+    {
+        float screenRatio = (float)Screen.safeArea.width / Screen.safeArea.height;
+        playableArea.localScale = playableArea.localScale * AspectHelper.GetScaleFactor(screenRatio);
+    }
+
+    private void SpawnPlayer()
+    {
+        Instantiate(playerPrefab, playableArea);
+    }
+
+    
+    private void LoadLevelData()
+    {
+        //TODO: load data for spawning projectiles and stuff;
+    }
+}
---
+++ src/warrior-the-rise/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject toBeControlled;
+    public float movementSpeed = 1f;
+    private Joystick joystick;
+
+    private Vector2 upperBound;
+    private Vector2 lowerBound;
+
+    private void Awake()
+    {
+        if (toBeControlled == null)
+        {
+            Debug.LogError("[MobileInputHandler] toBeControlled is null!");
+        }
+
+        //Look for a joystick in the scene
+        joystick = FindObjectOfType<Joystick>();
+
+        if (joystick == null)
+        {
+            Debug.LogError("[MobileInputHandler] joystrick is null!");
+        }
+
+        upperBound = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+        lowerBound = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float horizontal = 0f;
+        float vertical = 0f;
+
+        if (Mathf.Abs(joystick.Horizontal) > 0.3)
+        {
+            horizontal = joystick.Horizontal;
+        }
+
+        if (Mathf.Abs(joystick.Vertical) > 0.3)
+        {
+            vertical = joystick.Vertical;
+        }
+
+        if (Mathf.Abs(horizontal) > 0 || Mathf.Abs(vertical) > 0)
+        {
+            Vector3 v = new Vector3(horizontal, vertical, 0);
+
+            v *= movementSpeed * Time.deltaTime;
+            
+            toBeControlled.transform.Translate(v);
+
+            FixBounds(toBeControlled.transform.position);
+        }
+
+    }
+
+    private void FixBounds(Vector3 position)
+    {
+        float playerSizeOffset = toBeControlled.transform.lossyScale.x / 2;
+
+        if (position.x + playerSizeOffset > upperBound.x)
+        {
+            position.x = upperBound.x - playerSizeOffset;
+        }
+
+        if (position.x - playerSizeOffset < lowerBound.x)
+        {
+            position.x = lowerBound.x + playerSizeOffset;
+        }
+
+        if (position.y + playerSizeOffset > upperBound.y)
+        {
+            position.y = upperBound.y - playerSizeOffset;
+        }
+
+        if (position.y - playerSizeOffset < lowerBound.y)
+        {
+            position.y = lowerBound.y + playerSizeOffset;
+        }
+
+        toBeControlled.transform.position = position;
+
+    }
+    
+}
